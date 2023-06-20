9f7fb0af9c9249e7661dd795383a0fd6a19459ca
zakaria.enjebara <zakaria.benjebara@bpifrance.fr>
2023-06-20 23:03:33 +0200

    implement add order item with quantity

--- src/main/java/fr/bpifrance/crafts/orders/factories/OrderItemFactory.java
+++ src/main/java/fr/bpifrance/crafts/orders/factories/OrderItemFactory.java
@@ -1,14 +1,19 @@
 package fr.bpifrance.crafts.orders.factories;
 
 import fr.bpifrance.crafts.orders.model.Order;
+import fr.bpifrance.crafts.orders.model.OrderItem;
 
 public class OrderItemFactory {
 
-    public static void createOrderItem(Order order, String sku, Double price) {
+    public static void createOrderItem(Order order, String sku, Double price, int quantity) {
         if (null == order || null == order.getItems()) {
             return;
         }
-        order.getItems().put(sku, price);
+        OrderItem orderItem = new OrderItem();
+        orderItem.setSku(sku);
+        orderItem.setPrice(price);
+        orderItem.setQuality(quantity);
+        order.getItems().add(orderItem);
     }
 
     private OrderItemFactory() {}
